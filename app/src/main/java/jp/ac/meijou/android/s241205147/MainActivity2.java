e4e62049844d68260c3fa162041108dd43fe322a
Shion Bandai <241205147@ccmailg.meijo-u.ac.jp>
2025-09-03 16:21:24 +0900

    Day3Count3Change

--- app/src/main/java/jp/ac/meijou/android/s241205147/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205147/MainActivity2.java
@@ -5,17 +5,39 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Optional;
 
 import jp.ac.meijou.android.s241205147.databinding.ActivityMain2Binding;
 import jp.ac.meijou.android.s241205147.databinding.ActivityMainBinding;
 
 public class MainActivity2 extends AppCompatActivity {
     private ActivityMain2Binding binding;
-
+    private final ActivityResultLauncher<Intent> getActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                switch (result.getResultCode()) {
+                    case RESULT_OK -> {
+                        Optional.ofNullable(result.getData())
+                                .map(data -> data.getStringExtra("ret"))
+                                .map(text -> "Result: " + text)
+                                .ifPresent(text -> binding.textView3.setText(text));
+                    }
+                    case RESULT_CANCELED -> {
+                        binding.textView3.setText("Result: Canceled");
+                    }
+                    default -> {
+                        binding.textView3.setText("Result: Unknown(" + result.getResultCode() + ")");
+                    }
+                }
+            }
+    );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +58,16 @@ public class MainActivity2 extends AppCompatActivity {
             intent.setAction(Intent.ACTION_VIEW);
             intent.setData(Uri.parse("https://www.yahoo.co.jp"));
             startActivity(intent);
+        });
+        binding.button18.setOnClickListener(View -> {
+            var text = binding.editTextText2.getText().toString();
+            var intent = new Intent(this, MainActivity4.class);
+            intent.putExtra("title",text);
+            startActivity(intent);
+        });
+        binding.button19.setOnClickListener(View -> {
+            var intent = new Intent(this, MainActivity4.class);
+            getActivityResult.launch(intent);
         });
     }
 }
