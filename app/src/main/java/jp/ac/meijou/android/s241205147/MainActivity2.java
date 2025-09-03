183410327c45d5399a8516f4f5da14310737965e
Shion Bandai <241205147@ccmailg.meijo-u.ac.jp>
2025-09-03 14:38:05 +0900

    Day3Count2Change

--- app/src/main/java/jp/ac/meijou/android/s241205147/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205147/MainActivity2.java
@@ -1,5 +1,7 @@
 package jp.ac.meijou.android.s241205147;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +10,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205147.databinding.ActivityMain2Binding;
+import jp.ac.meijou.android.s241205147.databinding.ActivityMainBinding;
+
 public class MainActivity2 extends AppCompatActivity {
+    private ActivityMain2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        binding.button4.setOnClickListener(View  -> {
+            var intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+        binding.button5.setOnClickListener(View -> {
+            var intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.yahoo.co.jp"));
+            startActivity(intent);
         });
     }
 }
